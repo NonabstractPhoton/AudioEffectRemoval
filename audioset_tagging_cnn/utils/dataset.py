205a6767ed9309d64aea91a76f9eaf21cd5a5cde
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 18:51:02 -0400

    fixed improt

--- audioset_tagging_cnn/utils/dataset.py
+++ audioset_tagging_cnn/utils/dataset.py
@@ -9,7 +9,7 @@ import h5py
 import librosa
 
 from utilities import (create_folder, get_filename, create_logging, pad_or_truncate)
-import audioset_tagging_cnn.utils.config as config
+import config
 
 
 def split_unbalanced_csv_to_partial_csvs(args):
