01c0f0973e73ae6590a05a1957e3974b7bb4d4dd
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 17:53:42 -0400

    Modified apply_fx to allow multiple effects

--- audioset_tagging_cnn/utils/config.py
+++ audioset_tagging_cnn/utils/config.py
@@ -2,7 +2,7 @@ sample_rate = 44100
 clip_samples = sample_rate * 2     # Audio clips are 2-second
 
 # Note: compressor, envelope, fuzz must be added; they are not in the IDMT-AUDIO-EFFECTS dataset
-labels = ['reverb', 'chorus', 'flanger', 'bitcrusher','equalizer','phaser','tremolo','vibrato','distortion', 'overdrive','compressor','no effect']
+labels = ['reverb', 'chorus', 'flanger', 'bitcrusher','equalizer','phaser','tremolo','distortion', 'overdrive','compressor','no_effect']
 
 classes_num = len(labels)
 '''
--- dataset_generation/apply_fx.py
+++ dataset_generation/apply_fx.py
@@ -3,6 +3,8 @@ import os
 import sox 
 from scipy.io.wavfile import read, write
 from pedalboard.io import AudioFile
+
+from pedalboard import Pedalboard, Distortion, Bitcrush
 import multiprocessing as mp
 
 def main():
@@ -13,92 +15,89 @@ def main():
         exec(config_code, globals())
 
     parser = argparse.ArgumentParser(prog='apply_fx',description='Apply Fx to .wav Datasets')
-    parser.add_argument('in_directory')
-    parser.add_argument('out_directory_root')
-    parser.add_argument('effect')
+    parser.add_argument('--in_directory')
+    parser.add_argument('--out_directory_root')
+    parser.add_argument('effects', nargs='+')
     args = parser.parse_args()
 
-    if (args.effect not in labels):
-        print("invalid effect")
-        return
-    if (args.effect == "vibrato"):
-        print("vibrato effect not supported yet")
-        return
     if (os.path.exists(args.in_directory) == False):
-        print("input directory does not exist")
-        return
+                print("input directory does not exist")
+                return
     if (os.path.exists(args.out_directory_root) == False):
         os.mkdir(args.out_directory_root)
 
-    target_dir = os.path.join(args.out_directory_root, args.effect)
-    if (not os.path.exists(target_dir)):
-        os.mkdir(target_dir)
-    
-    print("Applying {} effect to files in {}".format(args.effect,args.in_directory))
-    pedalboard_needed = False
-    tfm = sox.Transformer()
-    fx = callable
-    if (args.effect == 'chorus'):
-        tfm.chorus()
-    elif (args.effect == 'flanger'):
-        tfm.flanger(depth=5,regen=10,speed=.6)
-    elif (args.effect == 'reverb'):
-        tfm.reverb(60)
-    elif args.effect == 'equalizer':
-        tfm.equalizer(220,2,0)
-    elif (args.effect == 'phaser'):
-        tfm.phaser()
-    elif (args.effect == 'tremolo'):
-        tfm.tremolo()
-    elif (args.effect == 'distortion'):
-        from pedalboard import Pedalboard, Distortion
-        pedalboard_needed = True
-        fx = Pedalboard([Distortion()])
+    for effect in args.effects:
+        if (effect not in labels):
+            print("invalid effect")
+            continue      
+
+        target_dir = os.path.join(args.out_directory_root, args.effect)
+        if (not os.path.exists(target_dir)):
+            os.mkdir(target_dir)
         
-    elif (args.effect == 'bitcrusher'):
-        from pedalboard import Pedalboard, Bitcrush
-        pedalboard_needed = True
-        fx = Pedalboard([Bitcrush()])
+        print("Applying {} effect to files in {}".format(args.effect,args.in_directory))
+        pedalboard_needed = False
+        tfm = sox.Transformer()
+        fx = callable
+        if (args.effect == 'chorus'):
+            tfm.chorus()
+        elif (args.effect == 'flanger'):
+            tfm.flanger(depth=5,regen=10,speed=.6)
+        elif (args.effect == 'reverb'):
+            tfm.reverb(60)
+        elif args.effect == 'equalizer':
+            tfm.equalizer(220,2,0)
+        elif (args.effect == 'phaser'):
+            tfm.phaser()
+        elif (args.effect == 'tremolo'):
+            tfm.tremolo()
+        elif (args.effect == 'distortion'):
+            pedalboard_needed = True
+            fx = Pedalboard([Distortion()])
+            
+        elif (args.effect == 'bitcrusher'):
+            pedalboard_needed = True
+            fx = Pedalboard([Bitcrush()])
 
-    elif (args.effect == 'overdrive'):
-        tfm.overdrive(25,30)
-    elif args.effect == 'compressor':
-        tfm.compand()
+        elif (args.effect == 'overdrive'):
+            tfm.overdrive(25,30)
+        elif args.effect == 'compressor':
+            tfm.compand()
 
-    # Only list regular .wav files from the input directory to avoid directories causing IsADirectoryError
-    dir_list = [f for f in os.listdir(args.in_directory)
-				if os.path.isfile(os.path.join(args.in_directory, f)) and f.lower().endswith('.wav')]
+        # Only list regular .wav files from the input directory to avoid directories causing IsADirectoryError
+        dir_list = [f for f in os.listdir(args.in_directory)
+                    if os.path.isfile(os.path.join(args.in_directory, f)) and f.lower().endswith('.wav')]
 
-    if not dir_list:
-        print("No .wav files found in input directory:", args.in_directory)
-        return
+        if not dir_list:
+            print("No .wav files found in input directory:", args.in_directory)
+            return
 
-    proc_count = min(mp.cpu_count(), len(dir_list))
-    dir_sublists = [dir_list[i::proc_count] for i in range(proc_count)]
+        proc_count = min(mp.cpu_count(), len(dir_list))
+        dir_sublists = [dir_list[i::proc_count] for i in range(proc_count)]
 
-    def apply_pedalboard_to_files(sub_list, fx=fx, in_dir=args.in_directory, target_dir=target_dir):
-        for filename in sub_list:
-            print("Processing file: {}".format(os.path.join(in_dir,filename)))
-            with AudioFile(os.path.join(in_dir,filename)) as f:
-                with AudioFile(os.path.join(target_dir,filename), 'w', f.samplerate, f.num_channels) as out_f:
-                    audio = f.read(f.frames)
-                    effected_audio = fx(audio, f.samplerate)
-                    out_f.write(effected_audio)
+        def apply_pedalboard_to_files(sub_list, fx=fx, in_dir=args.in_directory, target_dir=target_dir):
+            for filename in sub_list:
+                print("Processing file: {}".format(os.path.join(in_dir,filename)))
+                with AudioFile(os.path.join(in_dir,filename)) as f:
+                    with AudioFile(os.path.join(target_dir,filename), 'w', f.samplerate, f.num_channels) as out_f:
+                        audio = f.read(f.frames)
+                        effected_audio = fx(audio, f.samplerate)
+                        out_f.write(effected_audio)
 
-    def apply_tf_to_files(sub_list, tfm=tfm, in_dir=args.in_directory, target_dir=target_dir):
-        for filename in sub_list:
-            print("Processing file: {}".format(os.path.join(in_dir,filename)))
-            tfm.build_file(os.path.join(in_dir,filename),os.path.join(target_dir,filename))
+        def apply_tf_to_files(sub_list, tfm=tfm, in_dir=args.in_directory, target_dir=target_dir):
+            for filename in sub_list:
+                print("Processing file: {}".format(os.path.join(in_dir,filename)))
+                tfm.build_file(os.path.join(in_dir,filename),os.path.join(target_dir,filename))
 
-    processes = []
-    if (pedalboard_needed):
-        processes = [mp.Process(target=apply_pedalboard_to_files,args=(dir_sublists[i],)) for i in range(proc_count)]
-    else:
-        processes = [mp.Process(target=apply_tf_to_files,args=(dir_sublists[i],)) for i in range(proc_count)]
-    for p in processes:
-        p.start()   
-    for p in processes:
-        p.join()
-    
+        processes = []
+        if (pedalboard_needed):
+            processes = [mp.Process(target=apply_pedalboard_to_files,args=(dir_sublists[i],)) for i in range(proc_count)]
+        else:
+            processes = [mp.Process(target=apply_tf_to_files,args=(dir_sublists[i],)) for i in range(proc_count)]
+        for p in processes:
+            p.start()   
+        for p in processes:
+            p.join()
+        
 if __name__ == "__main__":
     main()
