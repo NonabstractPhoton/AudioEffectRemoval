5309d9fa2a9838ea9e1685757191ed6524f9f2c6
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 18:56:53 -0400

    fixed index

--- dataset_generation/pack_to_hdf5.sh
+++ dataset_generation/pack_to_hdf5.sh
@@ -1,3 +1,4 @@
+touch dataset/hdf5s/waveforms/$1.h5
 python audioset_tagging_cnn/utils/dataset.py pack_waveforms_to_hdf5 \
     --audios_dir=dataset/$1 
     --waveforms_hdf5_path=dataset/hdf5s/waveforms/$1.h5
