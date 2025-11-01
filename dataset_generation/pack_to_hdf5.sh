458977cd2f786cc6f42a947e80402aa836f49c9c
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 19:46:11 -0400

    typo fix

--- dataset_generation/pack_to_hdf5.sh
+++ dataset_generation/pack_to_hdf5.sh
@@ -1,3 +1,3 @@
 python audioset_tagging_cnn/utils/dataset.py pack_waveforms_to_hdf5 \
-    --audios_dir=$SCRATCH/AudioEffectsRemoval/dataset/$1 \
-    --waveforms_hdf5_path=$SCRATCH/AudioEffectsRemoval/dataset/hdf5s/waveforms/$1.h5
+    --audios_dir=$SCRATCH/AudioEffectRemoval/dataset/$1 \
+    --waveforms_hdf5_path=$SCRATCH/AudioEffectRemoval/dataset/hdf5s/waveforms/$1.h5
