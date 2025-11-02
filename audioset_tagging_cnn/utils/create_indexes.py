37acaad58de4ebb6ac8647056c6eb33d93e60b34
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 22:16:25 -0400

    fix audio counting

--- audioset_tagging_cnn/utils/create_indexes.py
+++ audioset_tagging_cnn/utils/create_indexes.py
@@ -28,7 +28,7 @@ def create_indexes(args):
 
     with h5py.File(waveforms_hdf5_path, 'r') as hr:
         with h5py.File(indexes_hdf5_path, 'w') as hw:
-            audios_num = len(os.listdir(waveforms_hdf5_path))
+            audios_num = len(hr['audio_name'])
             hw.create_dataset('audio_name', data=hr['audio_name'][:], dtype='S20')
             hw.create_dataset('target', data=hr['target'][:], dtype=np.bool)
             hw.create_dataset('hdf5_path', data=[waveforms_hdf5_path.encode()] * audios_num, dtype='S200')
