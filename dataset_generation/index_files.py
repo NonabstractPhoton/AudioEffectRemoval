9d31426546f17dc8ae5f157bb6fea054c7a21d9f
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 22:11:29 -0400

    multiline removal

--- dataset_generation/index_files.py
+++ dataset_generation/index_files.py
@@ -9,9 +9,7 @@ args = parser.parse_args()
 
 # temporary solution
 load_config.load_config(globals())
-query = """python3 audioset_tagging_cnn/utils/create_indexes.py create_indexes 
---waveforms_hdf5_path={}.h5
---indexes_hdf5_path={}.h5"""
+query = "python3 audioset_tagging_cnn/utils/create_indexes.py create_indexes --waveforms_hdf5_path={}.h5 --indexes_hdf5_path={}.h5"
 
 for label in labels:
     os.system(query.format(os.path.join(args.waveforms_dir, label),
