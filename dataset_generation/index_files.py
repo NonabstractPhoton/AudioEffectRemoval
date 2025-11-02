1beacb211bdac2d2d40eba431ccb706883041efc
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-01 22:06:14 -0400

    fix paths

--- dataset_generation/index_files.py
+++ dataset_generation/index_files.py
@@ -1,11 +1,18 @@
 import os
 import load_config
+import argparse
+
+parser = argparse.ArgumentParser(prog='index_files',description='Create Indexes for HDF5 Waveform Datasets')
+parser.add_argument('--waveforms_dir')
+parser.add_argument('--indexes_dir')
+args = parser.parse_args()
 
 # temporary solution
 load_config.load_config(globals())
 query = """python3 audioset_tagging_cnn/utils/create_indexes.py create_indexes 
---waveforms_hdf5_path=dataset/hdf5s/waveforms/{}.h5
---indexes_hdf5_path=dataset/hdf5s/indexes/{}.h5"""
+--waveforms_hdf5_path={}.h5
+--indexes_hdf5_path={}.h5"""
 
 for label in labels:
-    os.system(query.format(label, label))   
+    os.system(query.format(os.path.join(args.waveforms_dir, label),
+                           os.path.join(args.indexes_dir, label)))   
