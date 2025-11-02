1335a0f49c2cedb31d13312351233b21cc40ff43
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2025-11-02 00:01:03 -0400

    improved packing script

--- dataset_generation/pack_to_hdf5.sh
+++
@@ -1,3 +0,0 @@
-python audioset_tagging_cnn/utils/dataset.py pack_waveforms_to_hdf5 \
-    --audios_dir=$SCRATCH/AudioEffectRemoval/dataset/$1 \
-    --waveforms_hdf5_path=$SCRATCH/AudioEffectRemoval/dataset/hdf5s/waveforms/$1.h5
